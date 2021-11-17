978ef18af86f47619a9a3e194a9c446a888d4160
Julian Williams <julian.th.williams@gmail.com>
2021-11-16 22:08:34 -0800

    Initial with some work cause I am bad at committing often

---
+++ Develop/src/components/About.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+
+function About() {
+  const [landing, setLanding] =
+    useState();
+    //Does the bio and that jazz go here?
+}
+
+export default About;
